e9109cdcaa4c3e46a085f66d63646f08dbfc5b67
facte sun <shems.chelgoui@gmail.com>
2023-06-19 09:45:21 +0200

    yaaa

---
+++ src/app/State.ts
@@ -0,0 +1,7 @@
+export enum State {
+    NotStarted = 'Not Started',
+    Working = 'Working',
+    Faulted = 'Faulted',
+    Terminated = 'Terminated',
+    Unknown = 'Unknown'
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { State } from './State';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'composer';
+  myArray: Array<number> = [];
+  randomNumber: number = Math.random();
+  state: State = State.Working;
+  constructor() {
+    this.myArray
+    for (let i = 0; i <= 100; i++) {
+      if (i % 2 == 0) {
+        this.myArray.push(i);
+      } else continue;
+    }
+
+    console.log(this.randomNumber);
+  }
 }
